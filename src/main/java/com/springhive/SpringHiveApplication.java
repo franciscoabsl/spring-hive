bd8ec2eccf85753ab462e43776f463013a4d3bcd
Francisco Lima <franciscosouzalima@gmail.com>
2026-04-27 18:05:42 -0300

    chore: initial Spring Boot 3.5.14 project setup

---
+++ src/main/java/com/springhive/SpringHiveApplication.java
@@ -0,0 +1,13 @@
+package com.springhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springhive/SpringHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
